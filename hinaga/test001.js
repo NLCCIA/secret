47130bb1eb9becbb29262efc360e5d5e74a0c529
NLCCIA <116537861+NLCCIA@users.noreply.github.com>
2023-07-23 15:04:44 +0900

    Update test001.js

--- hinaga/test001.js
+++ hinaga/test001.js
@@ -8,14 +8,14 @@ let Sheet1_json     = XLSX.utils.sheet_to_json( Sheet1 )   // シート1のデ�
 // (例)シート1のセルA1の値をコンソールに出力します
 var r1        = Sheet1['B2'].v
 console.log( `紅　先鋒：\n${Sheet1B2}` )
-let r2        = Sheet1['C2'].v
+var r2        = Sheet1['C2'].v
 console.log( `紅　中堅：\n${Sheet1C2}` )
-let r3        = Sheet1['D2'].v
+var r3        = Sheet1['D2'].v
 console.log( `紅　大将：\n${Sheet1D2}` )
 
-let w1        = Sheet1['B3'].v
+var w1        = Sheet1['B3'].v
 console.log( `白　先鋒：\n${Sheet1B3}` )
-let w2       = Sheet1['C3'].v
+var w2       = Sheet1['C3'].v
 console.log( `白　中堅：\n${Sheet1C3}` )
-let w3        = Sheet1['D3'].v
+var w3        = Sheet1['D3'].v
 console.log( `白　大将：\n${Sheet1D3}` )
